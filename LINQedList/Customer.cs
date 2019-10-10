51b00390b4622f2ee23d2cc5d8ff2152925e294d
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-10-10 15:59:28 -0500

    linq exercises in progress

---
+++ LINQedList/Customer.cs
@@ -0,0 +1,9 @@
+namespace LINQedList
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
+}
